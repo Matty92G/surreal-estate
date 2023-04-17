bda30e394a85dc17db432a407beb1a92b2fa3114
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-17 21:29:47 +0100

    Still broken, require useState to work

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -7,9 +7,25 @@ const initialState = {
   },
 };
 const [fields, setFields] = useState(initialState.fields);
+const handleAddProperty = (event) => {
+  event.preventDefault();
+  console.log(fields);
+};
+const handleFieldChange = (event) => {
+  setFields({ ...fields, [event.target.name]: event.target.value });
+};
 const AddProperty = () => (
   <div className="add-property">
-    <form>
+    <form onSubmit={handleAddProperty}>
+      <label htmlFor="title">
+        Title
+        <input
+          id="title"
+          name="title"
+          value={fields.title}
+          onChange={handleFieldChange}
+        />
+      </label>
       <button type="submit">Add</button>
     </form>
   </div>
