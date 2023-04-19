c1b875bcdc635a341e3ebc81d398675c9fa016c3
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-19 21:31:07 +0100

    Added more keys to object in AddProperty.js and added select and input forms for them

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -5,6 +5,12 @@ const AddProperty = () => {
   const initialState = {
     fields: {
       title: '',
+      type: '',
+      bedrooms: '',
+      bathrooms: '',
+      price: '',
+      city: 'Manchester',
+      email: '',
     },
   };
   const [fields, setFields] = useState(initialState.fields);
@@ -24,6 +30,89 @@ const AddProperty = () => {
             id="title"
             name="title"
             value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="city">
+          City
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
+        <label htmlFor="type">
+          Type
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+        <label htmlFor="bedrooms">
+          Bedrooms
+          <select
+            id="bedrooms"
+            name="bedrooms"
+            value={fields.bedrooms}
+            onChange={handleFieldChange}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7+">7+</option>
+          </select>
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms
+          <select
+            id="bathrooms"
+            name="bathrooms"
+            value={fields.bathrooms}
+            onChange={handleFieldChange}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4+">4+</option>
+          </select>
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            id="price"
+            name="price"
+            type="number"
+            min="0"
+            step="1000"
+            value={fields.price}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="email">
+          eMail
+          <input
+            id="email"
+            name="email"
+            value={fields.email}
             onChange={handleFieldChange}
           />
         </label>
