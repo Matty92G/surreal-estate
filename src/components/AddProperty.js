2e38297679bd55a023f5ec338662404f2da0a4e0
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-19 21:02:36 +0100

    Fixed issue, required return in function

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,34 +1,36 @@
 import '../styles/add-property.css';
 import { React, useState } from 'react';
 
-const initialState = {
-  fields: {
-    title: '',
-  },
+const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: '',
+    },
+  };
+  const [fields, setFields] = useState(initialState.fields);
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+  return (
+    <div className="add-property">
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+          <input
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
-const [fields, setFields] = useState(initialState.fields);
-const handleAddProperty = (event) => {
-  event.preventDefault();
-  console.log(fields);
-};
-const handleFieldChange = (event) => {
-  setFields({ ...fields, [event.target.name]: event.target.value });
-};
-const AddProperty = () => (
-  <div className="add-property">
-    <form onSubmit={handleAddProperty}>
-      <label htmlFor="title">
-        Title
-        <input
-          id="title"
-          name="title"
-          value={fields.title}
-          onChange={handleFieldChange}
-        />
-      </label>
-      <button type="submit">Add</button>
-    </form>
-  </div>
-);
 
 export default AddProperty;
